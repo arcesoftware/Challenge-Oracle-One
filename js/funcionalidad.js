6b23111263e976e172dd3ed4d1a6cd4e9729759f
Juan Arce <77249136+arcesoftware@users.noreply.github.com>
2023-01-19 16:56:18 -0600

    Update funcionalidad.js

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -1,5 +1,5 @@
-const msj = document.querySelector("#textarea"); // Textarea del mensaje a Encriptar/Desencriptar
-const btnEncriptar = document.querySelector("#Encriptar"); // Botón para Encriptar
+const msj = document.querySelector("#textarea"); // Textarea del mensaje a encriptar/desencriptar
+const btnEncriptar = document.querySelector("#encriptar"); // Botón para encriptar
 const btnDesencriptar = document.querySelector("#desencriptar"); // Botón para desencriptar
 const btnCopiar = document.querySelector("#copiar"); // Botón para copiar el mensaje resultante
 const msjFinal = document.querySelector("#resultado"); // Textarea para mostrar el mensaje resultante
@@ -22,11 +22,9 @@ const msjEncriptar = txt => {
   }
 };
 
-let msjEncriptado = '';
-
-const Encriptar= txt => {
-  // Función para Encriptar un mensaje completo
-  msjEncriptado= txt.replace(/a|e|i|o|u/g, msjEncriptar); // Reemplaza cada letra por su versión Encriptda
+const encriptar = txt => {
+  // Función para encriptar un mensaje completo
+  const msjEncriptado = txt.replace(/a|e|i|o|u/g, msjEncriptar); // Reemplaza cada letra por su versión encriptada
   toggleElementVisibility('ocultar', false); // Oculta el elemento con id 'ocultar'
   toggleElementVisibility('resultado', true); // Muestra el elemento con id 'resultado'
   toggleElementVisibility('copiar', true); // Muestra el elemento con id 'copiar'
@@ -50,23 +48,23 @@ const msjDesencriptar = txt => {
   }
 };
 
-const Desencriptar = txt => {
-  const msjDesEncriptado = txt.replace(/ia|enter|imes|ober|ufat/g, mjsDesencriptar);
-  if (txt === msjEncriptado) return msjFinal;
+const desencriptar = txt => {
+  const msjDesencriptado = txt.replace(/ia|enter|imes|ober|ufat/g, msjDesencriptar);
+  if(txt === msjEncriptado) return msjFinal;
   else return msjDesencriptado;
 };
 
 btnEncriptar.addEventListener("click", event => {
   event.preventDefault();
   const txt = msj.value;
-  const msjSeguro = Encriptar(txt);
+  const msjSeguro = encriptar(txt);
   msjFinal.value = msjSeguro;
 });
 
 btnDesencriptar.addEventListener("click", event => {
   event.preventDefault();
   const txt = msj.value;
-  const msjSeguro = Desencriptar(txt);
+  const msjSeguro = desencriptar(txt);
   msjFinal.value = msjSeguro;
 });
 
